c7edb7ba48277925db96da3b06ff12a7a2f33a20
Oyetola David <tolafandpro@gmail.com>
2023-02-14 16:13:13 +0100

    url link is corrected in the API orders folder

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,86 +1,151 @@
 import styles from "../../styles/orders.module.css";
 import Image from "next/image";
+// import { getServerSideProps } from './../product/[id]';
+import axios from "axios";
 
-const Order = () => {
-    const status = 0;
+const Order = ({ order }) => {
+  const status = order.status;
 
-    const statusClass = (index) => {
-            if (index-status < 1) return styles.done
-            if (index-status === 1) return styles.inProgress
-            if (index-status > 1) return styles.inCompleted
-    };
+  const statusClass = (index) => {
+    if (index - status < 1) return styles.done;
+    if (index - status === 1) return styles.inProgress;
+    if (index - status > 1) return styles.inCompleted;
+  };
 
-    return ( 
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <div className={styles.row}>
-                    <table className={styles.table}>
-                        {/* <tr className={styles.trTitle}>
-                            <th>Order ID</th>
-                            <th>Customer</th>
-                            <th>Address</th>
-                            <th>Total</th>
-                        </tr> */}
-                        <tr className={styles.tr}>
-                            <td>
-                                <span className={styles.id}>123456677</span>
-                            </td>
-                            <td>
-                                <span className={styles.cusName}>
-                                    Oyetola David
-                                </span>
-                            </td>
-                            <td>
-                            <span className={styles.address}>
-                                George Creasent Ogba. Lagos
-                            </span>
-                            </td>
-                            <td><span className={styles.total}>$46.99</span></td>
-                        </tr>
-                    </table>
-                </div>
-                <div className={styles.row}>
-                    <div className={statusClass(0)}>
-                        <Image src="/images/paymentImg.png" width={50} height={50} alt="paymentimg" />
-                        <span>Payment</span>
-                        <div className={styles.checkIcon}>
-                            <Image className={styles.chkImg} src="/images/checkImg.png" width={40} height={40} alt="checkimg" />
-                        </div>
-                    </div>
-                    <div className={statusClass(1)}>
-                        <Image src="/images/preparing.png" width={50} height={50} alt="paymentimg" />
-                        <span>Preparing</span>
-                        <div className={styles.checkIcon}>
-                            <Image className={styles.chkImg} src="/images/checkImg.png" width={40} height={40} alt="checkimg" />
-                        </div>
-                    </div>
-                    <div className={statusClass(2)}>
-                        <Image src="/images/intransit.png" width={50} height={50} alt="paymentimg" />
-                        <span>In Transit</span>
-                        <div className={styles.checkIcon}>
-                            <Image className={styles.chkImg} src="/images/checkImg.png" width={40} height={40} alt="checkimg" />
-                        </div>
-                    </div>
-                    <div className={statusClass(3)}>
-                        <Image src="/images/pizzacheck.png" width={50} height={50} alt="paymentimg" />
-                        <span>Delivered</span>
-                        <div className={styles.checkIcon}>
-                            <Image className={styles.chkImg} src="/images/checkImg.png" width={40} height={40} alt="checkimg" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.right}>
-                <div className={styles.rightWrapper}>
-                        <h2 className={styles.title}>Cart Total</h2>
-                        <div><b className={styles.priceListText}>Subtotal:</b> $46</div>
-                        <div><b className={styles.priceListText}>Discount:</b> $0</div>
-                        <div><b className={styles.priceListText}>Total:</b> $46</div>
-                </div>
-                        <button disabled className={styles.button}>Paid</button>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.row}>
+          <table className={styles.table}>
+            {/* <tbody></tbody> */}
+            <tr className={styles.trTitle}>
+              <th>Order ID</th>
+              <th>Customer</th>
+              <th>Address</th>
+              <th>Total</th>
+            </tr>
+
+            <tr className={styles.tr}>
+              <td>
+                <span className={styles.id}>123456677{order._id}</span>
+              </td>
+              <td>
+                <span className={styles.cusName}>
+                  Oyetola David{order.customer}
+                </span>
+              </td>
+              <td>
+                <span className={styles.address}>
+                  George Creasent Ogba. Lagos{order.address}
+                </span>
+              </td>
+              <td>
+                <span className={styles.total}>$46.99{order.total}</span>
+              </td>
+            </tr>
+          </table>
         </div>
-     );
-}
- 
+        <div className={styles.row}>
+          <div className={statusClass(0)}>
+            <Image
+              src="/images/paymentImg.png"
+              width={50}
+              height={50}
+              alt="paymentimg"
+            />
+            <span>Payment</span>
+            <div className={styles.checkIcon}>
+              <Image
+                className={styles.chkImg}
+                src="/images/checkImg.png"
+                width={40}
+                height={40}
+                alt="checkimg"
+              />
+            </div>
+          </div>
+          <div className={statusClass(1)}>
+            <Image
+              src="/images/preparing.png"
+              width={50}
+              height={50}
+              alt="paymentimg"
+            />
+            <span>Preparing</span>
+            <div className={styles.checkIcon}>
+              <Image
+                className={styles.chkImg}
+                src="/images/checkImg.png"
+                width={40}
+                height={40}
+                alt="checkimg"
+              />
+            </div>
+          </div>
+          <div className={statusClass(2)}>
+            <Image
+              src="/images/intransit.png"
+              width={50}
+              height={50}
+              alt="paymentimg"
+            />
+            <span>In Transit</span>
+            <div className={styles.checkIcon}>
+              <Image
+                className={styles.chkImg}
+                src="/images/checkImg.png"
+                width={40}
+                height={40}
+                alt="checkimg"
+              />
+            </div>
+          </div>
+          <div className={statusClass(3)}>
+            <Image
+              src="/images/pizzacheck.png"
+              width={50}
+              height={50}
+              alt="paymentimg"
+            />
+            <span>Delivered</span>
+            <div className={styles.checkIcon}>
+              <Image
+                className={styles.chkImg}
+                src="/images/checkImg.png"
+                width={40}
+                height={40}
+                alt="checkimg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.rightWrapper}>
+          <h2 className={styles.title}>Cart Total</h2>
+          <div>
+            <b className={styles.priceListText}>Subtotal:</b> $46
+          </div>
+          <div>
+            <b className={styles.priceListText}>Discount:</b> $0
+          </div>
+          <div>
+            <b className={styles.priceListText}>Total:</b> $46
+          </div>
+        </div>
+        <button disabled className={styles.button}>
+          Paid
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(`http://localhost:3000/api/orders/${params.id}`);
+  return {
+    props: { order: res.data },
+  };
+};
+
 export default Order;
